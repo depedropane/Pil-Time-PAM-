abd6ed57ebc49fa2ccd3303cadeb53685ddad828
depedropane <depedrompane@gmail.com>
2026-05-31 17:17:07 +0700

    hosting

--- auth-service(dinonaktifkan)/internal/ports/outbound/nakes_repository.go
+++
@@ -1,8 +0,0 @@
-package outbound
-
-import "auth-service/internal/domain"
-
-// NakesRepository mendefinisikan kontrak akses data nakes
-type NakesRepository interface {
-	GetByEmail(email string) (*domain.Nakes, error)
-}
